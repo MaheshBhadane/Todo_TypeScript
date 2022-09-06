8ad121df513fcd3505c42e336229694815206e1e
MaheshBhadane <mahesh.bhadane@torinit.ca>
2022-09-06 14:07:06 +0530

    Added functionalities

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,12 @@
  
 const TodoItem: React.FC<{item: Todo; onRemoveTodo: () => void }> = (props) => {
-    return (<><li>{props.item.text} <button onClick={props.onRemoveTodo}>X</button></li> </>);
+    return (
+    <>
+    <li>
+        {props.item.text} 
+        <button onClick={props.onRemoveTodo}>X</button>
+    </li>
+    </>);
 };
 
 export default TodoItem;
